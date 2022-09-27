f9948b58760aa2bd977750454d4c8cf81330260b
sabrinaporto <sabrinapmota@yahoo.com.br>
2022-09-27 13:17:16 -0300

    Start do projeto

---
+++ trocadepontos/src/main/java/com/miniprojeto/trocadepontos/TrocadepontosApplication.java
@@ -0,0 +1,13 @@
+package com.miniprojeto.trocadepontos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrocadepontosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrocadepontosApplication.class, args);
+	}
+
+}
---
+++ trocadepontos/src/test/java/com/miniprojeto/trocadepontos/TrocadepontosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniprojeto.trocadepontos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrocadepontosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
