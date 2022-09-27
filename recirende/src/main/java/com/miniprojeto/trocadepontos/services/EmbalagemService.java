440aa04093a1e6d2bb2935e0459b33fc349b32bd
mauriciobadoco <mauriciobadoco@hotmail.com>
2022-09-27 15:51:14 -0300

    Create Controller and Service Embalagem

--- recirende/src/main/java/com/miniprojeto/trocadepontos/services/EmbalagemService.java
+++ recirende/src/main/java/com/miniprojeto/trocadepontos/services/EmbalagemService.java
@@ -1,14 +1,35 @@
 package com.miniprojeto.trocadepontos.services;
 
+import com.miniprojeto.trocadepontos.model.EmbalagemModel;
+import com.miniprojeto.trocadepontos.repository.IEmbalagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmbalagemService {
 
+    @Autowired
+    private IEmbalagemRepository repository;
 
+    public List<EmbalagemModel> mostrarEmbalagens(){
+        return repository.findAll();
+    }
 
+    public EmbalagemModel cadastrarEmbalagem(EmbalagemModel embalagemModel) {
+        return repository.save(embalagemModel);
+    }
+
+    public EmbalagemModel  alterarEmbalagem(EmbalagemModel embalagemModel){
+        return  repository.save(embalagemModel);
+    }
+
+    public void deletarEmbalagem(Long idEmbalagem){
+        repository.deleteById(idEmbalagem);
+    }
 
 
 }
+
