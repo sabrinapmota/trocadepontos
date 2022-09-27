5cdf41e845af7a84b228c2e23f1197af63759ccb
pauloren7 <paulorenan1515@gmail.com>
2022-09-27 15:52:29 -0300

    Adicionando DTO

--- recirende/src/main/java/com/miniprojeto/trocadepontos/model/UsuarioModel.java
+++ recirende/src/main/java/com/miniprojeto/trocadepontos/model/UsuarioModel.java
@@ -14,15 +14,15 @@ import javax.validation.constraints.Email;
 @NoArgsConstructor
 @Table(name = "TB_USUARIO")
 public class UsuarioModel {
-private long id;
+    private long id;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-@Column(nullable = false, unique = true, length = 30)
+    @Column(nullable = false, unique = true, length = 30)
     private String nome;
     @Column(nullable = false, unique = true, length = 11)
     private String cpf;
     @Column(nullable = false, unique = true, length = 20)
-    private  String email;
+    private String email;
     @Column(nullable = false, unique = false, length = 255)
-    private  String endereco;
+    private String endereco;
 }
