deb965a194142cef6d0cb4f341e110f549687b2d
mauriciobadoco <mauriciobadoco@hotmail.com>
2022-09-29 00:18:51 -0300

    add: Controller Tests

--- recirende/src/test/java/com/miniprojeto/trocadepontos/controller/UsuarioControllerTest.java
+++ recirende/src/test/java/com/miniprojeto/trocadepontos/controller/UsuarioControllerTest.java
@@ -3,11 +3,11 @@ package com.miniprojeto.trocadepontos.controller;
 import com.miniprojeto.trocadepontos.model.EmbalagemModel;
 import com.miniprojeto.trocadepontos.model.UsuarioModel;
 import com.miniprojeto.trocadepontos.services.EmbalagemService;
+import com.miniprojeto.trocadepontos.services.UsuarioServices;
 import io.restassured.http.ContentType;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -19,52 +19,51 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-public class EmbalagemControllerTest {
+class UsuarioControllerTest {
 
     @Autowired
-    private EmbalagemController embalagemController;
+    private UsuarioController usuarioController;
 
     @MockBean
-    private EmbalagemService embalagemService;
+    private UsuarioServices usuarioServices;
 
     @BeforeEach
     public void setUp(){
-        RestAssuredMockMvc.standaloneSetup(this.embalagemController);
+        RestAssuredMockMvc.standaloneSetup(this.usuarioController);
     }
 
     @Test
-    public void buscarEmbalagensTest(){
-        List<EmbalagemModel> lista = new ArrayList<>();
+    void buscarUsuarioTest() {
+        List<UsuarioModel> lista = new ArrayList<>();
 
-        when(this.embalagemService.mostrarEmbalagens())
+        when(this.usuarioServices.buscarTodos())
                 .thenReturn(lista);
-
-        RestAssuredMockMvc
-                .given()
-                .accept(ContentType.JSON)
-                .when()
-                .get("/embalagens")
-                .then()
-                .statusCode(HttpStatus.OK.value());
-    }
-
-    @Test
-    public void buscarEmbalagemIdTest() {
-
-        UsuarioModel usuarioModel = new UsuarioModel();
-        EmbalagemModel embalagemModel = new EmbalagemModel(1L, "Doritos", 123456 , LocalDate.now(), BigDecimal.valueOf(10.00),usuarioModel);
-
-        when(this.embalagemService.buscarId(1L))
-                .thenReturn(Optional.of(embalagemModel));
 
         RestAssuredMockMvc
                 .given()
                     .accept(ContentType.JSON)
                 .when()
-                    .get("/embalagens/{idEmbalagem}",1L)
+                    .get("/usuarios")
+                .then()
+                    .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void buscarUserIdTest() {
+        UsuarioModel usuarioModel = new UsuarioModel();
+
+        when(this.usuarioServices.buscarId(1L))
+                .thenReturn(Optional.of(usuarioModel));
+
+        RestAssuredMockMvc
+                .given()
+                    .accept(ContentType.JSON)
+                .when()
+                    .get("/usuarios/{idUsuario}",1L)
                 .then()
                     .statusCode(HttpStatus.OK.value());
     }
